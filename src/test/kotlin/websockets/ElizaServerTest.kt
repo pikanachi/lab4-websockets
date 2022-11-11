389b7fe1a5e7b9f3853cafe126d87a23e2358d4f
Nacho Ortega <74309113+pikanachi@users.noreply.github.com>
2022-11-11 21:08:03 +0100

    Fixed detekt issues

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -1,11 +1,9 @@
 @file:Suppress("NoWildcardImports")
 package websockets
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*
