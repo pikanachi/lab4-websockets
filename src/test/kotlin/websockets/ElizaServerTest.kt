06a9570ac74f99569b204733403e14f40093219d
Nacho Ortega <74309113+pikanachi@users.noreply.github.com>
2022-11-11 21:00:02 +0100

    Tests done

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -1,7 +1,9 @@
 @file:Suppress("NoWildcardImports")
 package websockets
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
@@ -37,7 +39,6 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -46,8 +47,12 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        // Mirar si hay exactamente 4 mensajes (3 open + 1 Eliza)
+        // assertEquals(4, list.size)
+        // No sabes si vas a tener 4 o 5 mensajes puede haber mandado más de un mensaje
+        assertTrue(list.size > 3)
+        // Llega el mensaje con 'allways' miro si el cuarto coincide con lo que debe responder
+        assertEquals("Can you think of a specific example?", list[3])
     }
 }
 
@@ -67,8 +72,10 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session) {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        // Si hay 3 mensajes (open) mando un mensaje
+        if (list.size == 3) {
+            // Mando un mensaje con el substring allways para comporobar si responde lo que debe
+            session.basicRemote.sendText("I'm always happy")
+        }
     }
 }
